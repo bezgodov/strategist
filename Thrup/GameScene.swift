a4d99199fe16a360ffc9b38f0efeeb2920feaade
bezgodov <maksim_bezgodov@mail.ru>
2017-11-06 09:28:31 +1000

    Пустой проект

---
+++ Thrup/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Thrup
+//
+//  Created by Максим Безгодов on 06.11.2017.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
