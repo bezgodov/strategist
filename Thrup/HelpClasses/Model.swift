01318791e39685b47d9ea816dfbff666f051daf1
bezgodov <maksim_bezgodov@mail.ru>
2018-01-14 16:30:17 +1000

    4,5,6 уровни

--- Thrup/HelpClasses/Model.swift
+++ Thrup/HelpClasses/Model.swift
@@ -25,7 +25,7 @@ class Model {
     var gameScene: GameScene!
     
     /// Общее количество уровней
-    var countLevels: Int = 5
+    var countLevels: Int = 6
     
     /// Текущий уровень
     var currentLevel: Int = 1
